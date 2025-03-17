be0aceb1822e80d0f652400fe362c07ec0ef74d5
Jieun <j.lim9304@gmail.com>
2025-03-17 18:51:50 +0900

    기타: 화면 레이아웃 적용 확인

---
+++ cafe/src/main/java/com/java/cafe/cafehome/HomeController.java
@@ -0,0 +1,13 @@
+package com.java.cafe.cafehome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "cafeHome/home";
+    }
+}
